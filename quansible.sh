82cdf1580e811118944a1c97bc5add063c329123
devd4n <daniel-schmitt-it@gmx.de>
2022-02-13 16:33:30 +0100

    Update quansible.sh > upgrade

--- quansible.sh
+++ quansible.sh
@@ -49,8 +49,8 @@ function upgrade() {
   echo "#!/bin/bash" > $ROOT_DIR/update_quansible.sh
   echo "cd $ROOT_DIR" >> $ROOT_DIR/update_quansible.sh
   echo "git clone $GITHUB_QUANSIBLE" >> $ROOT_DIR/update_quansible.sh
-  mv $ROOT_DIR/quansible.cfg.old $ROOT_DIR/quansible/
-  chmod +x $ROOT_DIR/quansible/quansible.sh
+  echo "mv $ROOT_DIR/quansible.cfg.old $ROOT_DIR/quansible/" >> $ROOT_DIR/update_quansible.sh
+  echo "chmod +x $ROOT_DIR/quansible/quansible.sh" >> $ROOT_DIR/update_quansible.sh
   chmod +x $ROOT_DIR/update_quansible.sh
 }
 
