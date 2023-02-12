d9c0c66b12ec1580cd7858060f0a9e0926787878
devd4n <daniel-schmitt-it@gmx.de>
2023-02-12 20:46:53 +0100

    remove variables from shell script and load it from file

--- quansible.sh
+++ quansible.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
 
-USER_ANSIBLE="ansible_admin"
-
-DIR_QUANSIBLE=$(pwd)
-ROOT_DIR="$(dirname "$DIR_QUANSIBLE")"
-DIR_ANSIBLE="$ROOT_DIR/ansible"
-DIR_INVENTORY="$DIR_ANSIBLE/private/inventory"
-DIR_ANSIBLE_CFG="$DIR_ANSIBLE/private"https://github.com/devd4n/quansible/blob/main/quansible.sh
-DIR_ANSIBLE_EXTRA_VARS="$DIR_ANSIBLE/private"
-DIR_ANSIBLE_REQUIREMENTS=$DIR_ANSIBLE
-
-QUANSIBLE_VENV="$ROOT_DIR/venv"
-GITHUB_QUANSIBLE="https://github.com/devd4n/quansible.git"
-ROLES_PATH="$ROOT_DIR/ansible/public/roles"
-
-SCRIPT_DIR=$(pwd)
-ANSIBLE_VERSION="7.2.0"
-
+# override default variables if custom config exists
 if test -f "{{ DIR_QUANSIBLE }}/quansible.cfg"; then
     echo "load custom quansible.cfg"
-    . "{{ DIR_QUANSIBLE }}//quansible.cfg"
+    . "{{ DIR_QUANSIBLE }}/quansible.cfg"
 fi
+# if no custom config exits use default config file
+elif test -f "{{ DIR_QUANSIBLE }}/default_quansible.cfg"; then
+    echo "load default quansible.cfg"
+    . "{{ DIR_QUANSIBLE }}/default_quansible.cfg"
 
+# update quansible environment
 function setup_ansible () {
+  # update user pip and initiate venv
   python3 -m pip install --upgrade pip
   python3 -m pip install virtualenv
   python3 -m venv $QUANSIBLE_VENV
-   
+  
+  # create necessary folders
   mkdir --parents $ROOT_DIR $DIR_ANSIBLE $DIR_INVENTORY $DIR_ANSIBLE_EXTRA_VARS
   
+  # write variables to ansible.cfg
   echo "---" > $DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml
   echo "root_dir: $ROOT_DIR" >> $DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml
   echo "dir_quansible: $DIR_QUANSIBLE" >> $DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml
@@ -40,10 +32,14 @@ function setup_ansible () {
   echo "[defaults]" > $DIR_ANSIBLE/ansible.cfg
   echo "inventory = $DIR_INVENTORY  ; list of hosts" >> $DIR_ANSIBLE/ansible.cfg
   echo "roles_path = $ROLES_PATH" >> $DIR_ANSIBLE/ansible.cfg
+
+  # update venv, install ansible in venv
   source $QUANSIBLE_VENV/bin/activate
   python3 -m pip install --upgrade pip
   python3 -m pip install wheel
   python3 -m pip install ansible==$ANSIBLE_VERSION
+
+  # run init playbook
   EXTRA_VARS="@$DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml"
   INIT_PLAYBOOK=$DIR_QUANSIBLE/quansible-init.yml
   ANSIBLE_CONFIG=$DIR_ANSIBLE_CFG/ansible.cfg
@@ -54,10 +50,12 @@ function setup_ansible () {
 function setup_roles () {
   source $QUANSIBLE_VENV/bin/activate
   cd $DIR_ANSIBLE
-  # Load all Roles from requirements.yml via ansible galaxy => ignore roles which didn't contain a meta/main.yml file.
+  # Load all Roles from requirements.yml via ansible galaxy
+  # ignore roles which didn't contain a meta/main.yml file.
   ansible-galaxy install -r "$DIR_ANSIBLE_REQUIREMENTS/requirements.yml" --ignore-errors
 }
 
+# creates the update_quansible.sh script
 function upgrade() {
   echo "#!/bin/bash" > $ROOT_DIR/update_quansible.sh
   echo "rm -r $ROOT_DIR/quansible" >> $ROOT_DIR/update_quansible.sh
@@ -76,14 +74,13 @@ function install_environment () {
   update-locale LANG=en_GB.UTF-8
   
   apt update
-  apt install sudo curl -y
-  # Install system requirements for virtualenv
-  apt install sudo python3-pip python3-venv -y
+  # Install system requirements and apps for virtualenv
+  apt install curl sudo python3-pip python3-venv -y
   # https://www.codegrepper.com/code-examples/shell/python+headers+are+missing+in+%2Fusr%2Finclude%2Fpython3.6m+%26quot%3Byum%26quot%3B
-  #https://stackoverflow.com/questions/31508612/pip-install-unable-to-find-ffi-h-even-though-it-recognizes-libffi
+  # https://stackoverflow.com/questions/31508612/pip-install-unable-to-find-ffi-h-even-though-it-recognizes-libffi
   apt install python-dev python3-dev libffi-dev -y
   # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=998232
-  # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   
   chown -R $USER_ANSIBLE:$USER_ANSIBLE $ROOT_DIR
 }
