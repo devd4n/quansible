29d6e0f7c1fc1252b51bf81ffb85e8543ccd0dfd
devd4n <devd4n@git.local>
2023-02-14 09:57:58 +0100

    gerneral code cleanup

--- quansible.sh
+++ quansible.sh
@@ -1,76 +1,28 @@
 #!/bin/bash
 
-# override default variables if custom config exists
-if test -f "{{ DIR_QUANSIBLE }}/quansible.cfg"; then
-    echo "load custom quansible.cfg"
-    . "{{ DIR_QUANSIBLE }}/quansible.cfg"
-fi
-# if no custom config exits use default config file
-elif test -f "{{ DIR_QUANSIBLE }}/default_quansible.cfg"; then
-    echo "load default quansible.cfg"
+if [[ test -f "{{ DIR_QUANSIBLE }}/quansible.cfg" ]]
+then
+  # override default variables if custom config exists
+  echo "load custom quansible.cfg"
+  . "{{ DIR_QUANSIBLE }}/quansible.cfg"
+elif [[ test -f "{{ DIR_QUANSIBLE }}/default_quansible.cfg" ]]
+then
+  # if no custom config exits use default config file
+  echo "load default quansible.cfg"
     . "{{ DIR_QUANSIBLE }}/default_quansible.cfg"
+else
+  echo "ERROR: something went wrong: no quansible.cfg file found"
+  exit
+fi
 
-# update quansible environment
-function setup_ansible () {
-  # update user pip and initiate venv
-  python3 -m pip install --upgrade pip
-  python3 -m pip install virtualenv
-  python3 -m venv $QUANSIBLE_VENV
-  
-  # create necessary folders
-  mkdir --parents $ROOT_DIR $DIR_ANSIBLE $DIR_INVENTORY $DIR_ANSIBLE_EXTRA_VARS
-  
-  # write variables to ansible.cfg
-  echo "---" > $DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml
-  echo "root_dir: $ROOT_DIR" >> $DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml
-  echo "dir_quansible: $DIR_QUANSIBLE" >> $DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml
-  echo "dir_ansible: $DIR_ANSIBLE" >> $DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml
-  echo "dir_inventory: $DIR_INVENTORY" >> $DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml
-  echo "roles_path: $ROLES_PATH" >> $DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml
-  echo "user_ansible_admin: $USER_ANSIBLE" >> $DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml
-
-  echo "[defaults]" > $DIR_ANSIBLE/ansible.cfg
-  echo "inventory = $DIR_INVENTORY  ; list of hosts" >> $DIR_ANSIBLE/ansible.cfg
-  echo "roles_path = $ROLES_PATH" >> $DIR_ANSIBLE/ansible.cfg
-
-  # update venv, install ansible in venv
-  source $QUANSIBLE_VENV/bin/activate
-  python3 -m pip install --upgrade pip
-  python3 -m pip install wheel
-  python3 -m pip install ansible==$ANSIBLE_VERSION
-
-  # run init playbook
-  EXTRA_VARS="@$DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml"
-  INIT_PLAYBOOK=$DIR_QUANSIBLE/quansible-init.yml
-  ANSIBLE_CONFIG=$DIR_ANSIBLE_CFG/ansible.cfg
-  ansible-playbook --extra-vars $EXTRA_VARS $INIT_PLAYBOOK --ask-become-pass
-  logout
-}
-
-function setup_roles () {
-  source $QUANSIBLE_VENV/bin/activate
-  cd $DIR_ANSIBLE
-  # Load all Roles from requirements.yml via ansible galaxy
-  # ignore roles which didn't contain a meta/main.yml file.
-  ansible-galaxy install -r "$DIR_ANSIBLE_REQUIREMENTS/requirements.yml" --ignore-errors
-}
-
-# creates the update_quansible.sh script
-function upgrade() {
-  echo "#!/bin/bash" > $ROOT_DIR/update_quansible.sh
-  echo "rm -r $ROOT_DIR/quansible" >> $ROOT_DIR/update_quansible.sh
-  echo "cd $ROOT_DIR" >> $ROOT_DIR/update_quansible.sh
-  echo "git clone $GITHUB_QUANSIBLE" >> $ROOT_DIR/update_quansible.sh
-  echo "chmod +x $ROOT_DIR/quansible/quansible.sh" >> $ROOT_DIR/update_quansible.sh
-  chmod +x $ROOT_DIR/update_quansible.sh
-}
-
+# install necessary dependencies and set system permissions
 function install_environment () {
   useradd -m $USER_ANSIBLE --shell /bin/bash
   echo "$USER_ANSIBLE  ALL=(ALL) NOPASSWD:ALL" >> sudo /etc/sudoers.d/$USER_ANSIBLE
-
+  
+  # ansible needs a UTF-8 locale
   locale-gen en_GB.UTF-8
-  locale-gen en_GB
+  #locale-gen en_GB
   update-locale LANG=en_GB.UTF-8
   
   apt update
@@ -82,7 +34,73 @@ function install_environment () {
   # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=998232
   #curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   
+  # give full ownership to the ansible user
   chown -R $USER_ANSIBLE:$USER_ANSIBLE $ROOT_DIR
+}
+
+# update quansible environment
+function update_ansible () {
+  # create necessary folders
+  mkdir --parents $ROOT_DIR $DIR_ANSIBLE $DIR_INVENTORY $DIR_ANSIBLE_EXTRA_VARS
+  
+  # write variables to file
+  cat <<-EOF > $DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml
+  root_dir: ${ROOT_DIR}
+  dir_quansible: ${DIR_QUANSIBLE}
+  dir_ansible: $DIR_ANSIBLE
+  dir_inventory: $DIR_INVENTORY
+  roles_repo: $POLES_REPO
+  roles_path: $ROLES_PATH
+  roles_repo_search: $ROLES_REPO_SEARCH
+  user_ansible_admin: $USER_ANSIBLE
+  EOF
+
+  # write variables to ansible.cfg
+  cat <<-EOF > $DIR_ANSIBLE/ansible.cfg
+  [defaults]
+  inventory = $DIR_INVENTORY
+  roles_path = $ROLES_PATH
+  EOF
+
+  # update user pip and initiate venv
+  python3 -m pip install --upgrade pip
+  python3 -m pip install virtualenv
+  python3 -m venv $QUANSIBLE_VENV
+  
+  # update venv, install ansible in venv
+  source $QUANSIBLE_VENV/bin/activate
+  python3 -m pip install --upgrade pip
+  python3 -m pip install wheel
+  python3 -m pip install ansible==$ANSIBLE_VERSION
+
+  # run init playbook
+  EXTRA_VARS="@$DIR_ANSIBLE_EXTRA_VARS/ansible_vars.yml"
+  INIT_PLAYBOOK=$DIR_QUANSIBLE/quansible-init.yml
+  ANSIBLE_CONFIG=$DIR_ANSIBLE_CFG/ansible.cfg
+  cd $DIR_ANSIBLE
+  ansible-playbook --extra-vars $EXTRA_VARS $INIT_PLAYBOOK --ask-become-pass
+  #deactivate
+  logout
+}
+
+# creates the update_quansible.sh script
+function upgrade() {
+  cat <<-EOF > $ROOT_DIR/update_quansible.sh
+  #!/bin/bash
+  rm -r $ROOT_DIR/quansible
+  cd $ROOT_DIR
+  git clone $GITHUB_QUANSIBLE
+  chmod +x $ROOT_DIR/quansible/quansible.sh
+  EOF
+  chmod +x $ROOT_DIR/update_quansible.sh
+}
+
+# Load all Roles from requirements.yml via ansible galaxy
+# ignore roles which didn't contain a meta/main.yml file.
+function load_roles () {
+  source $QUANSIBLE_VENV/bin/activate
+  cd $DIR_ANSIBLE
+  ansible-galaxy install -r "$DIR_ANSIBLE_REQUIREMENTS/requirements.yml" --ignore-errors
 }
 
 # Run function defined by parameter of this script (setup | init)
@@ -95,10 +113,10 @@ then
   exit
 elif [[ $1 == "update" ]]
 then
-  setup_ansible
+  update_ansible
 elif [[ $1 == "update-roles" ]]
 then
-  setup_roles
+  load_roles
 elif [[ $1 == "upgrade" ]]
 then
   upgrade
